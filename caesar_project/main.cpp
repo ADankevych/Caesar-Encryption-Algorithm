5f9009df51ef22d44a7764e28b7c1e49f8b872e7
adankevych <adankevych@kse.org.ua>
2024-06-17 01:32:09 +0300

    main for library

--- caesar_project/main.cpp
+++ caesar_project/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include "caesar.cpp"
+#include <dlfcn.h>
+using namespace std;
+
+typedef char* (*encrypt_ptr_t)(char*, int);
+typedef char* (*decrypt_ptr_t)(char*, int);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    void* handle = dlopen("./caesar.so", RTLD_LAZY);
+    if (handle == nullptr) {
+        cout << "Lib not found" << endl;
+        return -1;
+    }
+
+    encrypt_ptr_t encrypt_ptr = (encrypt_ptr_t)dlsym(handle, "encrypt");
+    decrypt_ptr_t decrypt_ptr = (decrypt_ptr_t)dlsym(handle, "decrypt");
+
+    char* rawText;
+    int key;
+    cout << "Enter the text you want to encrypt, and key: " << endl;
+    cin.getline(rawText, 100);
+    cin >> key;
+    cin.ignore();
+
+    char* encryptedText = encrypt_ptr(const_cast<char*>(rawText), key);
+    cout << "Encrypted text: " << encryptedText << endl;
+
+    cout << "Enter the text you want to decrypt, and key: " << endl;
+    char textForDecrypt[100];
+    int keyForDecrypt;
+    cin.getline(textForDecrypt, 100);
+    cin >> keyForDecrypt;
+    cin.ignore();
+
+    char* decryptedText = decrypt_ptr(encryptedText, key);
+    cout << "Decrypted text: " << decryptedText << endl;
+
+    dlclose(handle);
+
+    delete[] encryptedText;
+    delete[] decryptedText;
+
     return 0;
 }
