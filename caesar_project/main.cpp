7b42aa5bdf7c52a1689d74b4cbd3cee84c40d6ff
adankevych <adankevych@kse.org.ua>
2024-06-17 02:02:29 +0300

    added options for user

--- caesar_project/main.cpp
+++ caesar_project/main.cpp
@@ -16,27 +16,33 @@ int main() {
     encrypt_ptr_t encrypt_ptr = (encrypt_ptr_t)dlsym(handle, "encrypt");
     decrypt_ptr_t decrypt_ptr = (decrypt_ptr_t)dlsym(handle, "decrypt");
 
-    char rawText[100];
-    int key;
-    cout << "Enter the text you want to encrypt, and key: " << endl;
-    cin.getline(rawText, 100);
-    cin >> key;
+    int option;
+    cout << "Choose the option: 1 - use the encrypt, 2 - use the decrypt " << endl;
+    cin >> option;
     cin.ignore();
-    char* encryptedText = encrypt_ptr(rawText, key);
-    cout << "Encrypted text: " << encryptedText << endl;
-
-    cout << "Enter the text you want to decrypt, and key: " << endl;
-    char textForDecrypt[100];
-    int keyForDecrypt;
-    cin.getline(textForDecrypt, 100);
-    cin >> keyForDecrypt;
-    cin.ignore();
-
-    char* decryptedText = decrypt_ptr(textForDecrypt, keyForDecrypt);
-    cout << "Decrypted text: " << decryptedText << endl;
-
-    delete[] encryptedText;
-    delete[] decryptedText;
+    if (option == 1){
+        char rawText[100];
+        int key;
+        cout << "Enter the text you want to encrypt, and key: " << endl;
+        cin.getline(rawText, 100);
+        cin >> key;
+        cin.ignore();
+        char* encryptedText = encrypt_ptr(rawText, key);
+        cout << "Encrypted text: " << encryptedText << endl;
+        delete[] encryptedText;
+    } else if (option == 2){
+        cout << "Enter the text you want to decrypt, and key: " << endl;
+        char textForDecrypt[100];
+        int keyForDecrypt;
+        cin.getline(textForDecrypt, 100);
+        cin >> keyForDecrypt;
+        cin.ignore();
+        char* decryptedText = decrypt_ptr(textForDecrypt, keyForDecrypt);
+        cout << "Decrypted text: " << decryptedText << endl;
+        delete[] decryptedText;
+    } else {
+        cout << "Invalid option" << endl;
+    }
 
     dlclose(handle);
 
